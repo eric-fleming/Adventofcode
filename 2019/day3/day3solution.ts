2ecdd273557ac69dca00a510f363a5ee7b877453
mrfleming <ericfleming8@gmail.com>
2019-12-02 13:53:47 -0600

    setting up

---
+++ 2019/day3/day3solution.ts
@@ -0,0 +1,11 @@
+/*Dependent Modules*/
+import { readInput, inputToArray } from '../shared_functions/common';
+
+const rawInput = readInput('day3input.txt');
+
+
+function firstChallenge(){}
+function secondChallenge(){}
+
+//firstChallenge();
+//secondChallenge();
