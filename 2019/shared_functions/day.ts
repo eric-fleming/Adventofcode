7a5f5b47bfc0d00f22bc1bdd712e7f282ffe03a0
Eric <ericfleming8@gmail.com>
2019-12-03 10:14:04 -0600

    added main function

--- 2019/shared_functions/day.ts
+++ 2019/shared_functions/day.ts
@@ -1,5 +1,5 @@
 /*Dependent Modules*/
-import { readInput, inputToArray } from '../shared_functions/common';
+import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared_functions/common';
 
 const rawInput = readInput('day3input.txt');
 
@@ -7,5 +7,18 @@ const rawInput = readInput('day3input.txt');
 function firstChallenge() { }
 function secondChallenge() { }
 
-//firstChallenge();
-//secondChallenge();
+// main method to run the program
+function main(first: boolean, second: boolean) {
+    if (first) {
+        console.log('------  First Challenge Started ------');
+        firstChallenge();
+        console.log('------  Challend Completed -----------');
+    }
+    if (second) {
+        console.log('------  Second Challenge Started -----');
+        secondChallenge();
+        console.log('------  Challend Completed -----------');
+    }
+}
+
+main(true, true);
