4df641df1b1bcc3d4dfb1fb858bce99b017893b4
mrfleming <ericfleming8@gmail.com>
2019-12-03 01:42:50 -0600

    second challenge complete

--- 2019/day3/day3solution.js
+++ 2019/day3/day3solution.js
@@ -106,8 +106,6 @@ function buildPointPath(path) {
 function firstChallenge() {
     // get the instructions
     var paths = createPaths(inputArray);
-    // console.table(paths.first);
-    // console.table(paths.second);
     // Generate sequence of points in each path
     console.log("length of instructions 1: " + paths.first.length);
     console.log("length of instructions 2: " + paths.second.length);
@@ -115,32 +113,49 @@ function firstChallenge() {
     var listOfPoints2 = buildPointPath(paths.second);
     console.log("length of list 1: " + listOfPoints1.length);
     console.log("length of list 2: " + listOfPoints2.length);
-    console.log('------ printing test points ------');
-    PrintPoints(listOfPoints1, 10);
-    console.log('------ printing test points ------');
-    PrintPoints(listOfPoints2, 10);
-    /***/
     var path1Size = listOfPoints1.length;
     var path2Size = listOfPoints2.length;
     var intersection = [];
-    /**
     // I start at 1's because we dont want the origin.
-    for(let a = 1; a < path1Size; a++){
-        for(let b = 1; b < path2Size; b++){
-            if(listOfPoints1[a].equals(listOfPoints2[b])){
-                let p = listOfPoints1[a];
-                console.log(`Found a point of intersection : (${p.x}, ${p.y})`);
+    for (var a = 1; a < path1Size; a++) {
+        for (var b = 1; b < path2Size; b++) {
+            if (listOfPoints1[a].equals(listOfPoints2[b])) {
+                var p = listOfPoints1[a];
+                console.log("Found a point of intersection : (" + p.getX() + ", " + p.getY() + ")");
                 intersection.push(listOfPoints1[a]);
             }
         }
     }
     // Transform to the Manhattan distance
-    console.log(`Number of Intersectios : ${intersection.length}`);
-    const distances = intersection.map(point => Math.abs(point.x) + Math.abs(point.y));
+    console.log("Number of Intersectios : " + intersection.length);
+    var distances = intersection.map(function (point) { return Math.abs(point.getX()) + Math.abs(point.getY()); });
     console.table(distances.length);
-    const min = Math.min(...distances);
-    console.log(`The minimum distance is ${min}`); // */
+    var min = Math.min.apply(Math, distances);
+    console.log("The minimum distance is " + min); // */
 }
-function secondChallenge() { }
-firstChallenge();
-//secondChallenge();
+function secondChallenge() {
+    // get the instructions
+    var paths = createPaths(inputArray);
+    // Generate sequence of points in each path
+    var listOfPoints1 = buildPointPath(paths.first);
+    var listOfPoints2 = buildPointPath(paths.second);
+    var intersectionPairs = [];
+    // I start at 1's because we dont want the origin.
+    for (var a = 1; a < listOfPoints1.length; a++) {
+        for (var b = 1; b < listOfPoints2.length; b++) {
+            if (listOfPoints1[a].equals(listOfPoints2[b])) {
+                var p = listOfPoints1[a];
+                var pair = [listOfPoints1[a].getLength(), listOfPoints2[b].getLength()];
+                console.log("Found a point of intersection : (" + p.getX() + ", " + p.getY() + ")");
+                intersectionPairs.push(pair);
+            }
+        }
+    }
+    // Transform to the Manhattan distance
+    console.log("Number of Intersectios : " + intersectionPairs.length);
+    var intersctionTotalLengths = intersectionPairs.map(function (pair) { return pair[0] + pair[1]; });
+    var min = Math.min.apply(Math, intersctionTotalLengths);
+    console.log("The minimum length of both wires combined is " + min); // */
+}
+//firstChallenge();
+secondChallenge();
--- 2019/day3/day3solution.ts
+++ 2019/day3/day3solution.ts
@@ -128,9 +128,6 @@ function firstChallenge(){
     // get the instructions
     const paths = createPaths(inputArray);
     
-    // console.table(paths.first);
-    // console.table(paths.second);
-
     // Generate sequence of points in each path
     console.log(`length of instructions 1: ${paths.first.length}`);
     console.log(`length of instructions 2: ${paths.second.length}`);
@@ -138,34 +135,57 @@ function firstChallenge(){
     let listOfPoints2 = buildPointPath(paths.second);
     console.log(`length of list 1: ${listOfPoints1.length}`);
     console.log(`length of list 2: ${listOfPoints2.length}`);
-     
-    console.log('------ printing test points ------');
-    PrintPoints(listOfPoints1,10);
-    console.log('------ printing test points ------');
-    PrintPoints(listOfPoints2,10);
-    /***/
+    
     let path1Size = listOfPoints1.length;
     let path2Size = listOfPoints2.length;
     let intersection = [];
-    /** 
+    
     // I start at 1's because we dont want the origin.
     for(let a = 1; a < path1Size; a++){
         for(let b = 1; b < path2Size; b++){
             if(listOfPoints1[a].equals(listOfPoints2[b])){
                 let p = listOfPoints1[a];
-                console.log(`Found a point of intersection : (${p.x}, ${p.y})`);
+                console.log(`Found a point of intersection : (${p.getX()}, ${p.getY()})`);
                 intersection.push(listOfPoints1[a]);
             }
         }
     }
     // Transform to the Manhattan distance
     console.log(`Number of Intersectios : ${intersection.length}`);
-    const distances = intersection.map(point => Math.abs(point.x) + Math.abs(point.y));
+    const distances = intersection.map(point => Math.abs(point.getX()) + Math.abs(point.getY()));
     console.table(distances.length);
     const min = Math.min(...distances);
     console.log(`The minimum distance is ${min}`); // */
 }
-function secondChallenge(){}
+function secondChallenge(){
+    // get the instructions
+    const paths = createPaths(inputArray);
 
-firstChallenge();
-//secondChallenge();
+    // Generate sequence of points in each path
+    let listOfPoints1 = buildPointPath(paths.first);
+    let listOfPoints2 = buildPointPath(paths.second);
+    
+   
+    let intersectionPairs = [];
+
+    // I start at 1's because we dont want the origin.
+    for (let a = 1; a < listOfPoints1.length; a++) {
+        for (let b = 1; b < listOfPoints2.length; b++) {
+            if (listOfPoints1[a].equals(listOfPoints2[b])) {
+                let p = listOfPoints1[a];
+                let pair = [listOfPoints1[a].getLength(), listOfPoints2[b].getLength()];
+                console.log(`Found a point of intersection : (${p.getX()}, ${p.getY()})`);
+                intersectionPairs.push(pair);
+            }
+        }
+    }
+    // Transform to the Manhattan distance
+    console.log(`Number of Intersectios : ${intersectionPairs.length}`);
+    const intersctionTotalLengths = intersectionPairs.map(pair => pair[0] + pair[1]);
+    
+    const min = Math.min(...intersctionTotalLengths);
+    console.log(`The minimum length of both wires combined is ${min}`); // */
+}
+
+//firstChallenge();
+secondChallenge();
