3d78ddfc0e8dcb62031da846fe5926661be8ebfc
mrfleming <ericfleming8@gmail.com>
2019-12-03 21:54:24 -0600

    string input to object works

--- 2019/shared_functions/test.js
+++ 2019/shared_functions/test.js
@@ -3,12 +3,12 @@ exports.__esModule = true;
 /*Dependent Modules*/
 var common_1 = require("./common");
 var rawInput = common_1.readInput('testinput.txt');
-var data = common_1.inputToStringArray('testinput.txt', '\n');
+var data = common_1.inputToStringArray('testinput.txt', '\r\n');
 function test() {
     var dataObjects = [];
     for (var d = 0; d < data.length; d++) {
         var str = data[d];
-        var obj = common_1.stringInputToObject(str, ['index', 'quantity', 'dimension'], ['@', ':']);
+        var obj = common_1.stringInputToObject(str, ['order', 'quantity', 'dimension'], ['@', ':']);
         dataObjects.push(obj);
     }
     console.table(dataObjects);
--- 2019/shared_functions/test.ts
+++ 2019/shared_functions/test.ts
@@ -1,14 +1,14 @@
 /*Dependent Modules*/
-import { readInput, stringInputToObject, inputToArray, inputToNumberArray, inputToStringArray } from './common';
+import { readInput, stringInputToObject, inputToStringArray } from './common';
 
 const rawInput = readInput('testinput.txt');
-const data = inputToStringArray('testinput.txt','\n');
+const data = inputToStringArray('testinput.txt','\r\n');
 
 function test() { 
     let dataObjects: any[] = [];
     for(let d=0; d < data.length; d++){
         let str = data[d];
-        let obj = stringInputToObject(str,['index','quantity','dimension'],['@',':']);
+        let obj = stringInputToObject(str,['order','quantity','dimension'],['@',':']);
         dataObjects.push(obj);
     }
     console.table(dataObjects);
