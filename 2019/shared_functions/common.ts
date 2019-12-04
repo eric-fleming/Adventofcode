ed658bd0aea26a500ae55e3465dd8b2b65f23fd4
mrfleming <ericfleming8@gmail.com>
2019-12-03 20:24:35 -0600

    added string to object parsing function

--- 2019/shared_functions/common.ts
+++ 2019/shared_functions/common.ts
@@ -6,6 +6,33 @@ export function readInput(fileName: string) {
     return inputRead;
 }
 
+
+export function stringInputToObject(str: string, keys: string[], chars: string[]){
+    if(keys.length != (chars.length + 1)){
+        console.error('Miss match between key names and spliting characters');
+    }
+    let obj:any = {};
+    let start = 0
+    for(let n = 0; n < keys.length; n++){
+        let key = keys[n];
+        let stop;
+        // for most keys
+        if (n < (keys.length - 1)){
+            stop = str.indexOf(chars[n]);
+        }
+        // for the last key
+        else{
+            stop = str.length;
+        }
+        // extract substring, set key value pair
+        obj[key] = str.substring(start, stop);
+        // move the start
+        start = stop + 1;
+
+    }
+}
+
+
 export function inputToArray(input:string, type:string,char:string) {
     type = type.toLocaleLowerCase();
     const inputArray = input.split(char);
@@ -15,6 +42,10 @@ export function inputToArray(input:string, type:string,char:string) {
     }
     else if (type === 'string'){
         outputArray = inputArray.map((str) => String(str));
+    }
+    else{
+        console.log(`${type} is a not a primitive or Object type.`);
+        outputArray = [];
     }
     return outputArray;
 }
