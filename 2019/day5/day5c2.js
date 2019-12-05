2cc077a148e57ad38dec14cb22fa927f0d8859b4
mrfleming <ericfleming8@gmail.com>
2019-12-05 03:17:36 -0600

    creates 5-8, identifies 5-8, need to implement

--- 2019/day5/day5c2.js
+++ 2019/day5/day5c2.js
@@ -1,27 +1,22 @@
+"use strict";
+exports.__esModule = true;
 /*Dependent Modules*/
-import {
-    readInput, inputToArray, inputToNumberArray, inputToStringArray,
-    stringInputToObject, stringArrayToObjectArray
-} from '../shared_functions/common';
-
-const rawInput = readInput('day5input.txt');
+var common_1 = require("../shared_functions/common");
+var rawInput = common_1.readInput('day5input.txt');
 // Global variables
-let opCodeArray = [];
-let input: number;
-
+var opCodeArray = [];
+var input;
 // start or restart the machine
-function initializeMem(inputVal: number) {
-    opCodeArray = inputToArray(rawInput, 'number', ',');
+function initializeMem(inputVal) {
+    opCodeArray = common_1.inputToArray(rawInput, 'number', ',');
     input = inputVal; //1
 }
-
-
 // run the addition or multiplication routine
-function applyOldOpCode(c: number) {
+function applyOldOpCode(c) {
     //grabbing the locations
-    let left = opCodeArray[c + 1];
-    let right = opCodeArray[c + 2];
-    let output = opCodeArray[c + 3];
+    var left = opCodeArray[c + 1];
+    var right = opCodeArray[c + 2];
+    var output = opCodeArray[c + 3];
     // execute
     if (opCodeArray[c] === 1) {
         opCodeArray[output] = opCodeArray[left] + opCodeArray[right];
@@ -35,15 +30,13 @@ function applyOldOpCode(c: number) {
     }
     else if (opCodeArray[c] === 4) {
         // treat the input as a reference
-        console.log(`output : ${opCodeArray[left]}`);
+        console.log("output : " + opCodeArray[left]);
     }
 }
-
-
 function createOpCodeObj(code, c) {
-    let action = code % 100; // returns 1, 2, 3, 4, or 99
-    let params = (code - action) / 100; // clears the action
-    let opCode = {};
+    var action = code % 100; // returns 1, 2, 3, 4, or 99
+    var params = (code - action) / 100; // clears the action
+    var opCode = {};
     opCode['action'] = action;
     // set the param modes for the next inputs
     if (action === 99) {
@@ -56,63 +49,53 @@ function createOpCodeObj(code, c) {
             opCode['jump'] = 2;
             return opCode;
         }
-        else if (action === 5 || action === 6){
-            opCode['p2'] = Math.floor((params % 100) / 10);
-            opCode['jump'] = 3;
-            return opCode;
-        }
-        else if (action === 1 || action === 2 || action === 7 || action === 8) {
+        else if (action === 1 || action === 2) {
             opCode['p2'] = Math.floor((params % 100) / 10);
             opCode['p3'] = Math.floor(params / 100);
             opCode['jump'] = 4;
             return opCode;
         }
         else {
-            console.log(`Invalid opcode : ${action} at index ${c}`);
+            console.log("Invalid opcode : " + action + " at index " + c);
             return;
         }
     }
-
 }
-
-
 // zeros mean pass by reference
 function oldOpCode(opCodeObj) {
     // opcodes 1 and 2
-    let condtion12 = (opCodeObj['p1'] === 0 && opCodeObj['p2'] === 0 && opCodeObj['p3'] === 0);
+    var condtion12 = (opCodeObj['p1'] === 0 && opCodeObj['p2'] === 0 && opCodeObj['p3'] === 0);
     // opcodes 3 and 4
-    let condition34 = (opCodeObj['p1'] === 0 && opCodeObj['p2'] === undefined && opCodeObj['p3'] === undefined);
+    var condition34 = (opCodeObj['p1'] === 0 && opCodeObj['p2'] === undefined && opCodeObj['p3'] === undefined);
     return condtion12 || condition34;
 }
-
-
-
 function newOpCode(opCodeObj) {
     // true || undefined == true
     // so if the param does not exist it will not affect the result
     // ones mean pass by value
     return (opCodeObj['p1'] === 1 || opCodeObj['p2'] === 1 || opCodeObj['p2'] === 1);
 }
-
-function applyNewOpCode(opcode_idx: number, opCodeObj: any) {
+function applyNewOpCode(opcode_idx, opCodeObj) {
     // cache for readability
-    let action = opCodeObj['action'];
+    var action = opCodeObj['action'];
     // cautionary check for malformed input
     if (opCodeObj['p3'] === 1) {
-        console.log('How in the world does this make any sense for the output address to NOT BE an address??');
+        console.log('How in the world does this make any sense for the output address to not be an address');
     }
     else if (action === 1 || action === 2) {
         //staging the locations
-        let left: number, right: number;
-        let output: number = opCodeArray[opcode_idx + 3];
+        var left = void 0, right = void 0;
+        var output = opCodeArray[opcode_idx + 3];
         if (opCodeObj['p1'] === 1) {
             left = opCodeArray[opcode_idx + 1];
-        } else {
+        }
+        else {
             left = opCodeArray[opCodeArray[opcode_idx + 1]];
         }
         if (opCodeObj['p2'] === 1) {
             right = opCodeArray[opcode_idx + 2];
-        } else {
+        }
+        else {
             right = opCodeArray[opCodeArray[opcode_idx + 2]];
         }
         // execute
@@ -129,27 +112,23 @@ function applyNewOpCode(opcode_idx: number, opCodeObj: any) {
     }
     else if (action === 4) {
         // output the literal input value
-        console.log(`output : ${opCodeArray[opcode_idx + 1]}`);
+        console.log("output : " + opCodeArray[opcode_idx + 1]);
     }
 }
-
-
-
-function firstChallenge(init: number) {
+function firstChallenge(init) {
     initializeMem(init);
-    let maxLength = opCodeArray.length;
-
+    var maxLength = opCodeArray.length;
     // run through the opCodeArray
-    let c = 0
-    let i: number;
-    let optype: string;
+    var c = 0;
+    var i;
+    var optype;
     while (c < maxLength) {
         // extract from Intcode and make handling object
-        let code = opCodeArray[c];
+        var code = opCodeArray[c];
         // use this to verify what functions to call to modify the opCodeArray
-        let opCodeObj = createOpCodeObj(code, c);
+        var opCodeObj = createOpCodeObj(code, c);
         //console.table(opCodeObj);
-        let action = opCodeObj['action'];
+        var action = opCodeObj['action'];
         if (action === 99) {
             console.log('-- HALT --');
             break;
@@ -158,8 +137,12 @@ function firstChallenge(init: number) {
             optype = 'old';
             applyOldOpCode(c);
             // set for-loop incrementer
-            if (action === 1 || action === 2) { i = 4; }
-            else if (action === 3 || action === 4) { i = 2; }
+            if (action === 1 || action === 2) {
+                i = 4;
+            }
+            else if (action === 3 || action === 4) {
+                i = 2;
+            }
         }
         else if (newOpCode(opCodeObj)) {
             optype = 'new';
@@ -169,67 +152,43 @@ function firstChallenge(init: number) {
             applyNewOpCode(c, opCodeObj);
         }
         else {
-            console.log(`Error: unknown op-code!\n Found at index ${c}\n`);
+            console.log("Error: unknown op-code!\n Found at index " + c + "\n");
             console.table(opCodeObj);
             break;
         }
         //console.log(`completed opcode index ${c} with ${optype}`);
         c = c + i;
     }
-
 }
-
-
-
-
-
-function assemblyOpCode(opCodeObj){
-    let action = opCodeObj['action'];
-    return (action === 5 || action === 6 || action === 7 || action === 8);
-}
-
-
-
-function applyAssemblyOpCode(c:number, opCodeObj:any){
-
-
-}
-
-
-
-
-function secondChallenge(init: number) {
+function secondChallenge(init) {
     console.log('secretly 2... shhhhhhhhhhhhhhh ;)');
     initializeMem(init);
-    let maxLength = opCodeArray.length;
-
+    var maxLength = opCodeArray.length;
     // run through the opCodeArray
-    let c = 0
-    let i: number;
-    let optype: string;
+    var c = 0;
+    var i;
+    var optype;
     while (c < maxLength) {
         // extract from Intcode and make handling object
-        let code = opCodeArray[c];
+        var code = opCodeArray[c];
         // use this to verify what functions to call to modify the opCodeArray
-        let opCodeObj = createOpCodeObj(code, c);
+        var opCodeObj = createOpCodeObj(code, c);
         //console.table(opCodeObj);
-        let action = opCodeObj['action'];
+        var action = opCodeObj['action'];
         if (action === 99) {
             console.log('-- HALT --');
             break;
-        }
-        else if (assemblyOpCode(opCodeObj)){
-            optype = 'assembly';
-            applyAssemblyOpCode(c, opCodeObj);
-            // set for-loop incrementer
-            if (action === 7 || action === 8) { i = 4; }
         }
         else if (oldOpCode(opCodeObj)) {
             optype = 'old';
             applyOldOpCode(c);
             // set for-loop incrementer
-            if (action === 1 || action === 2) { i = 4; }
-            else if (action === 3 || action === 4) { i = 2; }
+            if (action === 1 || action === 2) {
+                i = 4;
+            }
+            else if (action === 3 || action === 4) {
+                i = 2;
+            }
         }
         else if (newOpCode(opCodeObj)) {
             optype = 'new';
@@ -239,7 +198,7 @@ function secondChallenge(init: number) {
             applyNewOpCode(c, opCodeObj);
         }
         else {
-            console.log(`Error: unknown op-code!\n Found at index ${c}\n`);
+            console.log("Error: unknown op-code!\n Found at index " + c + "\n");
             console.table(opCodeObj);
             break;
         }
@@ -247,11 +206,8 @@ function secondChallenge(init: number) {
         c = c + i;
     }
 }
-
-
-
 // main method to run the program
-function main(first: boolean, second: boolean) {
+function main(first, second) {
     if (first) {
         console.log('------  First Challenge Started ------');
         firstChallenge(1);
@@ -263,20 +219,11 @@ function main(first: boolean, second: boolean) {
         console.log('------  Challend Completed -----------');
     }
 }
-
 main(false, true);
-
-
-
-
-
-
-
 // old testing functions
-
 function testOpCodes() {
     // Testing opCode deduction
-    let index = -1;
+    var index = -1;
     console.table(createOpCodeObj(11101, index));
     console.table(createOpCodeObj(11102, index));
     console.table(createOpCodeObj(103, index));
@@ -285,15 +232,13 @@ function testOpCodes() {
     console.table(createOpCodeObj(10102, index));
     console.table(createOpCodeObj(1102, index));
     // all checks out
-
     // Testing boolean expressions
-    let temp = {};
+    var temp = {};
     temp['p1'] = 0;
     //temp['p2'] = 0;
     //temp['p3'] = 0;
     console.log(oldOpCode(temp));
     // works now
-
     initializeMem(1);
     console.log(opCodeArray[225]);
 }
