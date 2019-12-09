786a9f34a43a9839de7af75e12cc645b957ce3eb
mrfleming <ericfleming8@gmail.com>
2019-12-08 19:17:52 -0600

    removing some console.logs

--- 2019/day7/day7solution.js
+++ 2019/day7/day7solution.js
@@ -5,8 +5,8 @@ var common_1 = require("../shared_functions/common");
 var phasecodes_1 = require("./phasecodes");
 var amplifier_1 = require("./amplifier");
 var rawInput = common_1.readInput('day7input.txt');
-function firstChallenge(numOfAmplifiers, fileName) {
-    var phasecodes = phasecodes_1.generatePermutations(numOfAmplifiers);
+function firstChallenge(numOfAmplifiers, seed, fileName) {
+    var phasecodes = phasecodes_1.generatePermutations(numOfAmplifiers, seed);
     //console.table(phasecodes);
     var trusterOutputs = [];
     for (var p = 0; p < phasecodes.length; p++) {
@@ -22,7 +22,7 @@ function firstChallenge(numOfAmplifiers, fileName) {
             }
         }
         trusterOutputs.push(lastOutput);
-        console.log("---- Next Phase Code ----");
+        //console.log(`---- Next Phase Code ----`);
     }
     console.table(trusterOutputs);
     // trusterOuput list is complete, find max.
@@ -31,18 +31,56 @@ function firstChallenge(numOfAmplifiers, fileName) {
     console.log("The maximum Truster Output is " + max);
     console.log("=========================================");
 }
-function secondChallenge() { }
+function secondChallenge(numOfAmplifiers, seed, fileName) {
+    var phasecodes = phasecodes_1.generatePermutations(numOfAmplifiers, seed);
+    //console.table(phasecodes);
+    var trusterOutputs = [];
+    loop1: for (var p = 0; p < phasecodes.length; p++) {
+        var nextInputSignal = 0; // set this to the next outputSignal
+        var lastOutput = void 0;
+        loop2: for (var a = 0; a < numOfAmplifiers; a++) {
+            // set up
+            var phaseSetting = phasecodes[p][a];
+            var currentInput = nextInputSignal;
+            // calculate next output, future input
+            var Amp = new amplifier_1.Amplifier(phaseSetting, currentInput, fileName);
+            Amp.run();
+            nextInputSignal = Amp.getOutputSignal();
+            // halting condition??
+            if (nextInputSignal === undefined) {
+                lastOutput = currentInput;
+                break loop2;
+            }
+            // if you've reached Amp E
+            if (a === (numOfAmplifiers - 1)) {
+                lastOutput = nextInputSignal;
+                // go around again.
+                a = 0;
+            }
+        }
+        trusterOutputs.push(lastOutput);
+        //console.log(`---- Next Phase Code ----`);
+    }
+    console.table(trusterOutputs);
+    // trusterOuput list is complete, find max.
+    var max = Math.max.apply(Math, trusterOutputs);
+    console.log("=========================================");
+    console.log("The maximum Truster Output is " + max);
+    console.log("=========================================");
+}
 // main method to run the program
 function main(first, second) {
+    // parameters
+    // challenge (number of amps, seed value for phase settings, opcode instructions)
     if (first) {
         console.log('------  First Challenge Started ------');
-        firstChallenge(5, 'day7input.txt');
+        firstChallenge(5, 0, 'day7input.txt');
         console.log('------  Challend Completed -----------');
     }
     if (second) {
         console.log('------  Second Challenge Started -----');
-        secondChallenge();
+        secondChallenge(5, 5, 'day7input.txt');
         console.log('------  Challend Completed -----------');
     }
 }
-main(true, false);
+main(false, true);
--- 2019/day7/day7solution.ts
+++ 2019/day7/day7solution.ts
@@ -5,10 +5,9 @@ import { Amplifier } from './amplifier';
 const rawInput = readInput('day7input.txt');
 
 
-function firstChallenge(numOfAmplifiers:number, fileName: string) { 
-    const phasecodes = generatePermutations(numOfAmplifiers);
+function firstChallenge(numOfAmplifiers: number, seed: number, fileName: string) { 
+    const phasecodes = generatePermutations(numOfAmplifiers,seed);
     //console.table(phasecodes);
-    
     
     let trusterOutputs:number[] = [];
 
@@ -26,7 +25,7 @@ function firstChallenge(numOfAmplifiers:number, fileName: string) {
             }
         }
         trusterOutputs.push(lastOutput);
-        console.log(`---- Next Phase Code ----`);
+        //console.log(`---- Next Phase Code ----`);
     }
 
     console.table(trusterOutputs);
@@ -36,27 +35,72 @@ function firstChallenge(numOfAmplifiers:number, fileName: string) {
     console.log(`=========================================`);
     console.log(`The maximum Truster Output is ${max}`);
     console.log(`=========================================`);
-    
-
 }
 
 
 
 
-function secondChallenge() { }
+function secondChallenge(numOfAmplifiers: number, seed: number, fileName: string) { 
+    const phasecodes = generatePermutations(numOfAmplifiers, seed);
+    //console.table(phasecodes);
+
+    let trusterOutputs: number[] = [];
+    loop1:
+    for (let p = 0; p < phasecodes.length; p++) {
+        let nextInputSignal = 0; // set this to the next outputSignal
+        let lastOutput;
+        loop2:
+        for (let a = 0; a < numOfAmplifiers; a++) {
+            // set up
+            let phaseSetting = phasecodes[p][a];
+            let currentInput = nextInputSignal;
+
+            // calculate next output, future input
+            let Amp = new Amplifier(phaseSetting, currentInput, fileName);
+            Amp.run();
+            nextInputSignal = Amp.getOutputSignal();
+
+            // halting condition??
+            if (nextInputSignal === undefined) {
+                lastOutput = currentInput;
+                break loop2;
+            }
+            // if you've reached Amp E
+            if (a === (numOfAmplifiers - 1)) {
+                lastOutput = nextInputSignal;
+                // go around again.
+                a=0;
+            }
+        }
+        trusterOutputs.push(lastOutput);
+        //console.log(`---- Next Phase Code ----`);
+    }
+
+    console.table(trusterOutputs);
+
+    // trusterOuput list is complete, find max.
+    const max = Math.max(...trusterOutputs);
+    console.log(`=========================================`);
+    console.log(`The maximum Truster Output is ${max}`);
+    console.log(`=========================================`);
+}
 
 // main method to run the program
 function main(first: boolean, second: boolean) {
+    // parameters
+    // challenge (number of amps, seed value for phase settings, opcode instructions)
     if (first) {
         console.log('------  First Challenge Started ------');
-        firstChallenge(5, 'day7input.txt');
+        firstChallenge(5, 0, 'day7input.txt');
         console.log('------  Challend Completed -----------');
     }
     if (second) {
         console.log('------  Second Challenge Started -----');
-        secondChallenge();
+        secondChallenge(5, 5, 'day7input.txt');
         console.log('------  Challend Completed -----------');
     }
 }
 
-main(true, false);
+main(false, true);
+
+
