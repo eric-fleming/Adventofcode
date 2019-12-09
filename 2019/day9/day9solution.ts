ac98f1462607dc01bb26ffa1f2af4ea999816c37
mrfleming <ericfleming8@gmail.com>
2019-12-08 19:11:18 -0600

    initial upload

---
+++ 2019/day9/day9solution.ts
@@ -0,0 +1,35 @@
+/*Dependent Modules*/
+import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared_functions/common';
+
+const rawInput = readInput('day9input.txt');
+
+
+
+
+
+function firstChallenge() { 
+
+
+
+
+}
+
+
+
+function secondChallenge() { }
+
+// main method to run the program
+function main(first: boolean, second: boolean) {
+    if (first) {
+        console.log('------  First Challenge Started ------');
+        firstChallenge();
+        console.log('------  Challend Completed -----------');
+    }
+    if (second) {
+        console.log('------  Second Challenge Started -----');
+        secondChallenge();
+        console.log('------  Challend Completed -----------');
+    }
+}
+
+main(true, false);
---
+++ 2019/day9/testday9.ts
@@ -0,0 +1,3 @@
+/*Dependent Modules*/
+import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared_functions/common';
+import {} from './day9solution';
