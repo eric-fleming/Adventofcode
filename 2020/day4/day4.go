6b264445262a5ff02a7d58a759e40b3ce4d07c1e
Eric Fleming <ericfleming8@gmail.com>
2020-12-03 21:52:11 -0600

    chore: inital day 4 setup

---
+++ 2020/day4/day4.go
@@ -0,0 +1,31 @@
+package main
+
+//chore: inital day setup
+import (
+	"fmt"
+
+	common "../common"
+)
+
+func main() {
+
+	// Read the file
+	filename := "day4input.txt"
+	text := common.ReadInputText(filename)
+	common.Show(text, "string")
+
+	fmt.Println("-*-*-*-*--*-*-*-*-*--*-*-*-*-*--*-*-*-*-")
+	fmt.Println("-*-*-*-*-*- MERRY CHRISTMAS! -*-*-*-*-*-")
+	fmt.Println("-*-*-*-*--*-*-*-*-*--*-*-*-*-*--*-*-*-*-")
+	part1()
+	part2()
+
+}
+
+func part1() {
+	fmt.Println("\n-*-*-*-*-*- Part 1! -*-*-*-*-*-")
+}
+
+func part2() {
+	fmt.Println("\n-*-*-*-*-*- Part 2! -*-*-*-*-*-")
+}
