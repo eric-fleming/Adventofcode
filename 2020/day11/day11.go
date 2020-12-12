ee43fc46bfc89efe7f5ab0fa253832cdc5eb0465
Eric Fleming <ericfleming8@gmail.com>
2020-12-12 13:31:31 -0600

    refactor: padded input, makes adj count easier

--- 2020/day11/day11.go
+++ 2020/day11/day11.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	common "../common"
 )
@@ -15,15 +16,32 @@ func main() {
 	// Read the file
 	filename := "day11input.txt"
 	text := common.ReadInputText(filename)
+	paddedText := padWithEmptySpace(text)
+	common.Show(paddedText, "string")
 
-	//common.Show(text, "string")
 	//fmt.Printf("line:\t%v\n", text[0])
 	fmt.Println("-*-*-*-*--*-*-*-*-*--*-*-*-*-*--*-*-*-*-")
 	fmt.Println("-*-*-*-*-*- MERRY CHRISTMAS! -*-*-*-*-*-")
 	fmt.Println("-*-*-*-*--*-*-*-*-*--*-*-*-*-*--*-*-*-*-")
-	part1(text)
-	part2(text)
 
+	part1(paddedText)
+	part2(paddedText)
+
+}
+
+func padWithEmptySpace(text []string) []string {
+	rowlen := len(text[0]) + 2
+	size := len(text) + 2
+	paddedText := make([]string, size)
+	paddedText[0] = strings.Repeat(".", rowlen)
+	// Add empty space to make D8 lookup easier
+	// guard rails
+	for i, line := range text {
+		paddedText[i+1] = "." + line + "."
+	}
+	rowlen = len(paddedText[0])
+	paddedText[size-1] = strings.Repeat(".", rowlen)
+	return paddedText
 }
 
 func part1(inputText []string) {
