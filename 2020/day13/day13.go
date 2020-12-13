273ed2ad164fabdb0056e75e82ccd8929c2d73ac
Eric Fleming <ericfleming8@gmail.com>
2020-12-13 11:56:21 -0600

    feat: day 13 part 2 COMPLETED

--- 2020/day13/day13.go
+++ 2020/day13/day13.go
@@ -15,6 +15,7 @@ func main() {
 
 	// Read the file
 	filename := "day13input.txt"
+	//example := "example1.txt"
 	text := common.ReadInputText(filename)
 	//common.Show(templateForrest, "string")
 	start, _ := strconv.Atoi(strings.TrimSpace(text[0]))
@@ -29,7 +30,7 @@ func main() {
 		}
 	}
 
-	fmt.Printf("%v", nums)
+	fmt.Printf("%v\n", nums)
 	part1(nums, start)
 	part2(info)
 
@@ -64,16 +65,97 @@ func part1(nums []int, target int) {
 
 func part2(text []string) {
 	fmt.Println("\n-*-*-*-*-*- Part 2! -*-*-*-*-*-")
-	var nums []int
-	for i, item := range text {
-		if item == "x" {
-			nums[i] = 0
-		} else if item != "x" {
-			n, _ := strconv.Atoi(item)
+
+	// convert to []int
+	nums := make([]int, len(text))
+	fmt.Println(text)
+	for i := 0; i < len(text); i++ {
+		if text[i] == "x" {
+			nums[i] = 1
+		} else if text[i] != "x" {
+			n, _ := strconv.Atoi(strings.TrimSpace(text[i]))
 			nums[i] = n
 		}
 	}
+	fmt.Printf("nums = %v\n", nums)
 
-	fmt.Printf("nums = %v", nums)
+	// count the number of equations to solve in the system
+	count := 0
+	max := 1
+	for _, num := range nums {
+		if num != 1 {
+			max = max * num
+			count++
+		}
+	}
 
+	// create the list of mods to pass to solving function
+	mods := make([]common.Modulo, count)
+	place := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 1 {
+			mods[place] = *common.NewModulo(i, nums[i])
+			place++
+		}
+	}
+
+	// execute Chinese Remainder Theorem
+	answer := common.ChineseRemainderThm(mods)
+
+	reduced := answer.B % answer.M
+	fmt.Printf("%v\t===\t came from %v\n", reduced, answer.Statement)
+	fmt.Println("---- FINAL ANSWER ----")
+	final := max - reduced
+	fmt.Printf("%v", final)
+
+}
+
+func longwayPart2(text []string) {
+	fmt.Println("\n-*-*-*-*-*- Part 2! -*-*-*-*-*-")
+
+	//example := []string{"17", "x", "13", "19"}
+	//example2 := []string{"67", "7", "59", "61"}
+	//example3 := []string{"67", "7", "x", "59", "61"}
+	//text = example3
+
+	nums := make([]int, len(text))
+	fmt.Println(text)
+	for i := 0; i < len(text); i++ {
+		if text[i] == "x" {
+			nums[i] = 1
+		} else if text[i] != "x" {
+			n, _ := strconv.Atoi(strings.TrimSpace(text[i]))
+			nums[i] = n
+		}
+	}
+	fmt.Printf("nums = %v\n", nums)
+
+	base := 1 // 749,468,541,208,439
+	//           100,000,000,000,000
+	for _, num := range nums {
+		base = base * num
+	}
+	fmt.Printf("base # == %v\n", base)
+
+	//start := (100000000000000 / nums[0]) * nums[0]
+	for i := base; i > 0; i = i + 13 {
+		meetAllConditions := true
+		for d := 0; d < len(nums); d++ {
+			if ((i + d) % nums[d]) == 0 {
+				meetAllConditions = meetAllConditions && true
+			} else {
+				meetAllConditions = false
+			}
+		}
+		if i%10000000 == 0 {
+			fmt.Println("10 milli...")
+		}
+		if meetAllConditions {
+			fmt.Println("The time stamp that works for all...")
+			fmt.Printf("Is... %v", i)
+			break
+		}
+	}
+
+	fmt.Println("---- Fin! ----")
 }
