2f1462a799f7ad62df6757b68200e3740a7e8115
Eric Fleming <ericfleming8@gmail.com>
2020-12-12 18:08:55 -0600

    feat: part2 COMPLETED

--- 2020/day10/day10.go
+++ 2020/day10/day10.go
@@ -23,13 +23,12 @@ func main() {
 	fmt.Printf("%v\n", sortedNums)
 
 	part1(sortedNums)
-	part2()
+	part2(sortedNums)
 }
 
 func part1(adapters []int) {
 	fmt.Println("\n-*-*-*-*-*- Part 1! -*-*-*-*-*-")
 
-	//maxRating := adapters[len(adapters)-1] + 3
 	joltDiffs := map[int]int{
 		1: 0,
 		2: 0,
@@ -55,7 +54,51 @@ func part1(adapters []int) {
 
 }
 
-func part2() {
+func part2(adapters []int) {
 	fmt.Println("\n-*-*-*-*-*- Part 2! -*-*-*-*-*-")
+	fmt.Println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+	fmt.Println("-*-*-*- RECURSION PARTY! -*-*-*-")
 
+	//maxRating := adapters[len(adapters)-1] + 3
+	adapterPaths := dynamicAdapterPath(adapters)
+
+	fmt.Printf("Number of adapter combinations = %v", adapterPaths)
+
+}
+
+func dynamicAdapterPath(adapters []int) int {
+	paths := make([]int, len(adapters))
+	for i := 0; i < len(adapters); i++ {
+		paths[i] = 0
+	}
+
+	for i := 0; i < len(paths); i++ {
+		if i == 0 && adapters[0] <= 3 {
+			paths[0] = 1
+		} else if i == 1 && adapters[1] <= 3 {
+			paths[1] = 1 + paths[0]
+		} else if i == 2 && adapters[2] <= 3 {
+			paths[2] = 1 + paths[1] + paths[0]
+		} else if i >= 3 {
+			if testConnectionStep(adapters, i, i-1) {
+				paths[i] += paths[i-1]
+			}
+			if testConnectionStep(adapters, i, i-2) {
+				paths[i] += paths[i-2]
+			}
+			if testConnectionStep(adapters, i, i-3) {
+				paths[i] += paths[i-3]
+			}
+		}
+	}
+
+	return paths[len(paths)-1]
+}
+
+func testConnectionStep(adapters []int, joltIndex, nextIndex int) bool {
+	// Does the next step meet the criteria
+	if nextIndex < 0 {
+		return false
+	}
+	return (adapters[joltIndex]-adapters[nextIndex] <= 3)
 }
