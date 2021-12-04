399ec69f8906e66cfc5a51b3b667b33663d4af3a
eric-fleming <ericfleming8@gmail.com>
2021-12-04 14:49:34 -0600

    day 1 refactor, don't actually need to sum

--- 2021/day1/day1.ts
+++ 2021/day1/day1.ts
@@ -6,7 +6,7 @@ const depths = inputToNumberArray('input1.txt','\n');
 
 
 function part1(){
-    let increases = 0;
+    let increases: number = 0;
 
     for (let d = 1; d < depths.length; d++) {
         if (depths[d] > depths[d - 1]) {
@@ -19,28 +19,16 @@ function part1(){
 
 
 function part2(){
-    let increases = 0;
+    let increases: number = 0;
     for (let d = 3; d < depths.length; d++) {
-        let firstTriple = depths[d - 3] + depths[d - 2] + depths[d - 1];
-        let secondTriple = depths[d - 2] + depths[d - 1]+ depths[d];
+        let firstTriple = depths[d - 3];
+        let secondTriple = depths[d];
         if (secondTriple> firstTriple) {
             increases++;
         }
     }
     log(`There are ${increases} in the depths`);
 }
-
-
-function betterSecondChallenge() {
-    let increases = 0;
-    for (let d = 3; d < depths.length; d++) {
-        if (depths[d] > depths[d-3]) {
-            increases++;
-        }
-    }
-    log(`There are ${increases} in the depths`);
-}
-
 
 // main method to run the program
 function main(first: boolean, second: boolean) {
