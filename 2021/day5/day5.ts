24d5b6c820efd047dbf31761fe15e4219ebaeb93
eric-fleming <ericfleming8@gmail.com>
2021-12-04 13:35:29 -0600

    day 5 setup

---
+++ 2021/day5/day5.ts
@@ -0,0 +1,37 @@
+/*Dependent Modules*/
+import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared/common';
+const log = console.log;
+const rawInput = inputToStringArray('day5input.txt', '\n');
+//console.table(rawInput);
+
+function part1() {
+    
+    let answer;
+    log(`${answer}`);
+}
+
+function part2() {
+    
+    let answer;
+    log(`${answer}`);
+}
+
+// main method to run the program
+function main(first: boolean, second: boolean) {
+    if (first) {
+        log('--------------------------------------');
+        log('----------  First Challenge ----------');
+        part1();
+        log('--------------------------------------');
+        log('\n\n');
+    }
+    if (second) {
+        log('--------------------------------------');
+        log('----------  Second Challenge ---------');
+        part2();
+        log('--------------------------------------');
+        log('\n\n');
+    }
+}
+
+main(true, true);
