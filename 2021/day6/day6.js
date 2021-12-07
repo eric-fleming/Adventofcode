52717abe6b5810e4c7a1b7fd0474a9b5f09eb469
eric-fleming <ericfleming8@gmail.com>
2021-12-06 21:37:56 -0600

    day 6 clean up: how can recurance relations help?

--- 2021/day6/day6.js
+++ 2021/day6/day6.js
@@ -12,8 +12,7 @@ exports.__esModule = true;
 var common_1 = require("../shared/common");
 var log = console.log;
 var rawInput = (0, common_1.inputToNumberArray)('day6.input.txt', ',');
-//console.table(rawInput);
-function processDay(fishArray) {
+function processFishEachDay(fishArray) {
     var newfish = [];
     for (var f = 0; f < fishArray.length; f++) {
         if (fishArray[f] == 0) {
@@ -27,110 +26,25 @@ function processDay(fishArray) {
     fishArray = __spreadArray(__spreadArray([], fishArray, true), newfish, true);
     return fishArray;
 }
-function part1() {
-    var days = 80;
-    var fishes = rawInput;
+function part1(days) {
+    log("After ".concat(days, " days..."));
+    var fishes = __spreadArray([], rawInput, true);
     while (days > 0) {
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
-    //console.table(fishes);
-    log("There are ".concat(fishes.length, " in the population"));
+    log("Fish population is ".concat(fishes.length));
 }
-function lifetimeImpact(days, fishes) {
-    while (days > 0) {
-        fishes = processDay(fishes);
-        days--;
-    }
-    return fishes.length;
-}
-function fish(countdownVal, day) {
-    return {
-        day: day,
-        val: countdownVal
-    };
-}
-function addFishDepthFirst(seedFish) {
-    var population = 0;
-    var seedfish = seedFish;
-    var fishStack = [];
-    while (seedfish != undefined) {
-        //log(`new seed fish: [day, val] = ${seedfish.day} ${seedfish.val}`);
-        // add new fish to stack
-        while (seedfish.day > 0) {
-            if (seedfish.val > 0) {
-                seedfish.val -= 1;
-            }
-            else if (seedfish.val == 0) {
-                fishStack.push(fish(8, seedfish.day));
-                seedfish.val = 6;
-            }
-            seedfish.day -= 1;
-            if (seedfish.val > seedfish.day) {
-                //can't create any more
-                seedfish.day = 0;
-            }
-        }
-        // remove seed fish
-        if (seedfish.day == 0) {
-            population += 1;
-            seedfish = undefined;
-        }
-        // move to next seed fish
-        seedfish = fishStack.pop();
-    }
-    return population;
-}
-function part2() {
-    var totalPopulation = 0;
-    var date = 18;
-    var cache = new Map();
-    var startingFish = [3, 4, 3, 1, 2];
-    log("Starting fish...");
-    log("day: ".concat(date, "\t val: ").concat(startingFish[0]));
-    while (startingFish.length > 0) {
-        var seedFishValue = startingFish.pop();
-        if (cache.has(seedFishValue)) {
-            totalPopulation += cache.get(seedFishValue);
-        }
-        else {
-            var subPopulation = addFishDepthFirst(fish(seedFishValue, date));
-            totalPopulation += subPopulation;
-            cache.set(seedFishValue, subPopulation);
-        }
-    }
-    log("total population: ".concat(totalPopulation, " of fish"));
-    console.table(cache);
-}
-// main method to run the program
-function main(first, second) {
-    if (first) {
-        log('--------------------------------------');
-        log('----------  First Challenge ----------');
-        part1();
-        log('--------------------------------------');
-        log('\n\n');
-    }
-    if (second) {
-        log('--------------------------------------');
-        log('----------  Second Challenge ---------');
-        part2();
-        log('--------------------------------------');
-        log('\n\n');
-    }
-}
-//main(false, false);
-function cacheBuilder(val, days) {
+function cacheBuilder(days, val) {
     if (days == 0) {
         return 1;
     }
     var fishes = [val];
     //log('started while')
     while (days > 0) {
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
-    //log('finished while')
     return fishes.length;
 }
 function fishArray(day, val) {
@@ -140,14 +54,11 @@ function dynamicTable(dayLimit) {
     var table = new Map();
     var counter = 0;
     for (var day = dayLimit; day > 0; day--) {
-        //log('started '+day+' day');
         for (var val = 8; val >= 0; val--) {
-            var sub_population = cacheBuilder(val, day);
+            var sub_population = cacheBuilder(day, val);
             table.set(fishArray(day, val).toString(), sub_population);
             counter++;
-            //log(`counter at ${counter}`);
         }
-        //log(`finished day ${day}`);
     }
     return table;
 }
@@ -155,18 +66,18 @@ function decomposeStart() {
     var fishes = rawInput;
     var starts = new Map();
     for (var k = 0; k < fishes.length; k++) {
-        var fish_1 = fishes[k];
-        if (starts.has(fish_1)) {
-            var mapVal = starts.get(fish_1);
-            starts.set(fish_1, mapVal + 1);
+        var fish = fishes[k];
+        if (starts.has(fish)) {
+            var mapVal = starts.get(fish);
+            starts.set(fish, mapVal + 1);
         }
         else {
-            starts.set(fish_1, 1);
+            starts.set(fish, 1);
         }
     }
     return starts;
 }
-function part1withCache(days, cache, input) {
+function singleFishContribution(days, cache, input) {
     var fishes = input; //rawInput [3, 4, 3, 1, 2]
     var population = 0;
     while (days > 0) {
@@ -175,7 +86,6 @@ function part1withCache(days, cache, input) {
         var remainingFish = [];
         for (var k = 0; k < fishes.length; k++) {
             if (cache.has(fishArray(days, fishes[k]).toString())) {
-                //log('in cache')
                 population += cache.get(fishArray(days, fishes[k]).toString());
             }
             else {
@@ -187,49 +97,40 @@ function part1withCache(days, cache, input) {
             break;
         }
         //process remainng fish
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
-    //console.table(fishes);
-    //log(`There are ${population} in the population`);
     return population;
 }
-var duplicateStarts = decomposeStart();
-console.table(duplicateStarts);
-log(rawInput.length);
-/**
-log('---------------------')
-log('part 1 with cache\n')
-
-log('fish after 80 days = ' + part1withCache(256,128));
-log('---------------------')
-*/
-/*
-let arr = [8,1];
-log(arr.toString());
-
-console.table(cache);
-console.log(cache.has([8, 1].toString()));
-*/
-function realPart2() {
+function part2(days, cacheDays) {
     var duplicateStarts = decomposeStart();
+    var cached_table = dynamicTable(cacheDays);
     var sub_branch_populations = [];
-    log('caching started...');
-    var cache = dynamicTable(128);
-    log('caching complete');
     for (var key = 1; key <= 5; key++) {
-        log("sub brach ".concat(key, " started..."));
-        sub_branch_populations[key] = part1withCache(256, cache, [key]);
-        log("sub brach ".concat(key, " completed"));
+        sub_branch_populations[key] = singleFishContribution(days, cached_table, [key]);
     }
     var population = 0;
     for (var key = 1; key <= 5; key++) {
         var product = sub_branch_populations[key] * duplicateStarts.get(key);
         population += product;
     }
-    log("Total population = ".concat(population));
+    log("After ".concat(days, " days...\nThe fish population is ").concat(population));
 }
-log('---------------------');
-log('part 2 with cache\n');
-log('fish after 256 days = ');
-realPart2();
+// main method to run the program
+function main(first, second) {
+    if (first) {
+        log('--------------------------------------');
+        log('----------  First Challenge ----------');
+        part1(80);
+        log('--------------------------------------');
+        log('\n\n');
+    }
+    if (second) {
+        log('--------------------------------------');
+        log('----------  Second Challenge ---------');
+        part2(256, 128);
+        log('--------------------------------------');
+        log('\n\n');
+    }
+}
+main(true, true);
--- 2021/day6/day6.ts
+++ 2021/day6/day6.ts
@@ -1,11 +1,9 @@
-/*Dependent Modules*/
-import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
-import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared/common';
+import { inputToNumberArray } from '../shared/common';
 const log = console.log;
 const rawInput = inputToNumberArray('day6.input.txt', ',');
-//console.table(rawInput);
 
-function processDay(fishArray){
+
+function processFishEachDay(fishArray){
     let newfish = []
     for(let f=0; f<fishArray.length;f++){
         if(fishArray[f] == 0){
@@ -19,134 +17,27 @@ function processDay(fishArray){
     return fishArray;
 }
 
-function part1() {
-    let days = 80;
-    let fishes = rawInput;
+function part1(days) {
+    log(`After ${days} days...`)
+    let fishes = [...rawInput];
     while(days>0){
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
-
-    //console.table(fishes);
-    log(`There are ${fishes.length} in the population`);
-    
+    log(`Fish population is ${fishes.length}`);
 }
 
-function lifetimeImpact(days,fishes){
-    while (days > 0) {
-        fishes = processDay(fishes);
-        days--;
-    }
-
-    return fishes.length
-}
-
-function fish(countdownVal:number, day:number){
-    return {
-        day:day,
-        val: countdownVal
-    }
-}
-
-function addFishDepthFirst(seedFish) {
-    let population = 0;
-    let seedfish = seedFish;
-    let fishStack = [];
-    while(seedfish != undefined){
-        //log(`new seed fish: [day, val] = ${seedfish.day} ${seedfish.val}`);
-        // add new fish to stack
-        while(seedfish.day > 0){
-            if(seedfish.val > 0){
-                seedfish.val -= 1;
-            }
-            else if(seedfish.val == 0){
-                fishStack.push(fish(8,seedfish.day));
-                seedfish.val = 6;
-            }
-            seedfish.day -= 1;
-            if(seedfish.val > seedfish.day){
-                //can't create any more
-                seedfish.day=0;
-            }
-        }
-
-        // remove seed fish
-        if(seedfish.day == 0){
-            population += 1;
-            seedfish = undefined;
-        }
-
-        // move to next seed fish
-        seedfish = fishStack.pop();
-        
-    }
-
-    return population;
-}
-
-function part2(){
-    let totalPopulation = 0;
-    let date = 18;
-    const cache = new Map();
-    let startingFish = [3, 4, 3, 1, 2];
-    log(`Starting fish...`)
-    log(`day: ${date}\t val: ${startingFish[0]}`);
-
-    while(startingFish.length > 0){
-        let seedFishValue = startingFish.pop();
-
-        if (cache.has(seedFishValue)){
-            totalPopulation += cache.get(seedFishValue);
-        }
-        else{
-            let subPopulation = addFishDepthFirst(fish(seedFishValue, date));
-            totalPopulation += subPopulation;
-            cache.set(seedFishValue, subPopulation);
-        }
-        
-    }
-
-    log(`total population: ${totalPopulation} of fish`);
-    console.table(cache);
-
-
-
-}
-
-// main method to run the program
-function main(first: boolean, second: boolean) {
-    if (first) {
-        log('--------------------------------------');
-        log('----------  First Challenge ----------');
-        part1();
-        log('--------------------------------------');
-        log('\n\n');
-    }
-    if (second) {
-        log('--------------------------------------');
-        log('----------  Second Challenge ---------');
-        part2();
-        log('--------------------------------------');
-        log('\n\n');
-    }
-}
-
-//main(false, false);
-
-function cacheBuilder(val:number,days:number) {
+function cacheBuilder(days: number,val: number,) {
     if(days == 0){
         return 1;
     }
     let fishes = [val];
     //log('started while')
     while (days > 0) {
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
-    //log('finished while')
-
     return fishes.length;
-
 }
 
 function fishArray(day: number, val: number,){
@@ -158,14 +49,11 @@ function dynamicTable(dayLimit: number): Map<string, number>{
     let counter = 0
     
     for(let day=dayLimit; day>0; day--){
-        //log('started '+day+' day');
         for(let val=8;val>=0;val--){
-            let sub_population = cacheBuilder(val,day);
+            let sub_population = cacheBuilder(day,val);
             table.set(fishArray(day,val).toString(),sub_population);
             counter++;
-            //log(`counter at ${counter}`);
         }
-        //log(`finished day ${day}`);
     }
 
     return table;
@@ -184,25 +72,22 @@ function decomposeStart(){
             starts.set(fish,1);
         }
     }
-
     return starts
 }
 
-function part1withCache(days,cache,input){
+function singleFishContribution(days:number,cache:Map<string,number>,input:number[]){
 
     let fishes = input;//rawInput [3, 4, 3, 1, 2]
     let population = 0;
     
     while (days > 0) {
-
         //scan for fish in cache
         //move fish to new array
         let remainingFish = [];
         for(let k=0; k<fishes.length;k++){
             if(cache.has(fishArray(days,fishes[k]).toString())){
-                //log('in cache')
                 population += cache.get(fishArray(days, fishes[k]).toString());
-            } else{
+            } else {
                 remainingFish.push(fishes[k]);
             }
         }
@@ -212,50 +97,22 @@ function part1withCache(days,cache,input){
         }
 
         //process remainng fish
-        fishes = processDay(fishes);
+        fishes = processFishEachDay(fishes);
         days--;
     }
 
-    //console.table(fishes);
-    //log(`There are ${population} in the population`);
-    return population
+    return population;
 }
 
-let duplicateStarts = decomposeStart();
-console.table(duplicateStarts)
-log(rawInput.length)
 
-/** 
-log('---------------------')
-log('part 1 with cache\n')
-
-log('fish after 80 days = ' + part1withCache(256,128));
-log('---------------------')
-*/
-
-/*
-let arr = [8,1];
-log(arr.toString());
-
-console.table(cache);
-console.log(cache.has([8, 1].toString()));
-*/
-
-
-function realPart2(){
+function part2(days,cacheDays){
 
     let duplicateStarts = decomposeStart();
-    let sub_branch_populations:number[] = [];
-
-    log('caching started...');
-    let cache = dynamicTable(128);
-    log('caching complete');
+    let cached_table = dynamicTable(cacheDays);
+    let sub_branch_populations: number[] = [];
     
-
     for(let key=1;key<=5;key++){
-        log(`sub brach ${key} started...`);
-        sub_branch_populations[key] = part1withCache(256,cache,[key]);
-        log(`sub brach ${key} completed`);
+        sub_branch_populations[key] = singleFishContribution(days, cached_table,[key]);
     }
 
     let population = 0;
@@ -264,12 +121,27 @@ function realPart2(){
         population += product;
     }
 
-    log(`Total population = ${population}`);
+    log(`After ${days} days...\nThe fish population is ${population}`);
 
 }
 
-log('---------------------')
-log('part 2 with cache\n')
 
-log('fish after 256 days = ');
-realPart2()
+// main method to run the program
+function main(first: boolean, second: boolean) {
+    if (first) {
+        log('--------------------------------------');
+        log('----------  First Challenge ----------');
+        part1(80);
+        log('--------------------------------------');
+        log('\n\n');
+    }
+    if (second) {
+        log('--------------------------------------');
+        log('----------  Second Challenge ---------');
+        part2(256,128);
+        log('--------------------------------------');
+        log('\n\n');
+    }
+}
+
+main(true, true);
