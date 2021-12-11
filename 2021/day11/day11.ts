13f31a2c69a89d93320b33e5c0f3becca2206fc9
eric-fleming <ericfleming8@gmail.com>
2021-12-11 13:06:38 -0600

    day 11 setup

---
+++ 2021/day11/day11.ts
@@ -0,0 +1,40 @@
+/*Dependent Modules*/
+import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared/common';
+const log = console.log;
+const info = console.info; //(stuff)=>{}
+// day11input.txt
+// ex11.txt
+const rawInput = inputToStringArray('ex11.txt', '\n');
+//console.table(rawInput);
+
+function part1() {
+
+    let answer;
+    log(`${answer}`);
+}
+
+function part2() {
+
+    let answer;
+    log(`${answer}`);
+}
+
+// main method to run the program
+function main(first: boolean, second: boolean) {
+    if (first) {
+        log('--------------------------------------');
+        log('----------  First Challenge ----------');
+        log('--------------------------------------');
+        part1();
+        log('\n\n');
+    }
+    if (second) {
+        log('--------------------------------------');
+        log('----------  Second Challenge ---------');
+        log('--------------------------------------');
+        part2();
+        log('\n\n');
+    }
+}
+
+main(true, false);
