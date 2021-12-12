0e5f9955da5c69ef19d5b244c1f95d472ce5c453
eric-fleming <ericfleming8@gmail.com>
2021-12-11 22:41:34 -0600

    day 11 complete

--- 2021/day11/day11.ts
+++ 2021/day11/day11.ts
@@ -1,22 +1,118 @@
 /*Dependent Modules*/
-import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared/common';
+import { inputToStringArray } from '../shared/common';
+import { padNumberMatrix, Point, d8Neighborhood, convertToNumberMatrix } from '../shared/matrix';
 const log = console.log;
-const info = console.info; //(stuff)=>{}
-// day11input.txt
-// ex11.txt
-const rawInput = inputToStringArray('ex11.txt', '\n');
-//console.table(rawInput);
+const table = console.table;
 
-function part1() {
-
-    let answer;
-    log(`${answer}`);
+function increaseAll(paddedMatrix: number[][]): number[][] {
+    let rows = paddedMatrix.length;
+    let cols = paddedMatrix[0].length;
+    for (let r = 1; r < rows - 1; r++) {
+        for (let c = 1; c < cols - 1; c++) {
+            if (paddedMatrix[r][c] < 10) {
+                paddedMatrix[r][c] += 1
+            }
+        }
+    }
+    return paddedMatrix;
 }
 
-function part2() {
+function increaseNeighborhood(point: Point, matrix: number[][]): number[][] {
+    let neighborhood = d8Neighborhood(point);
+    neighborhood.forEach(point => {
+        if (matrix[point.y][point.x] < 10) {
+            matrix[point.y][point.x] += 1;
+        }
+    });
 
-    let answer;
-    log(`${answer}`);
+    return matrix;
+}
+
+function findFlashes(matrix: number[][]): number[][] {
+    let maxEnergyCoordinates: number[][] = []
+    let rows = matrix.length;
+    let cols = matrix[0].length;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (matrix[r][c] == 10) {
+                maxEnergyCoordinates.push([c, r]);
+            }
+        }
+    }
+    return maxEnergyCoordinates;
+}
+
+function resetFlashed(paddedMatrix: number[][]) {
+    let rows = paddedMatrix.length;
+    let cols = paddedMatrix[0].length;
+    for (let r = 1; r < rows - 1; r++) {
+        for (let c = 1; c < cols - 1; c++) {
+            if (paddedMatrix[r][c] == 10) {
+                paddedMatrix[r][c] = 0;
+            }
+        }
+    }
+    return paddedMatrix;
+}
+
+function compute(part, steps, source) {
+    const rawInput = inputToStringArray(source, '\r\n');
+    let cavern = [...rawInput];
+    let cavernMatrix: number[][] = padNumberMatrix(convertToNumberMatrix(cavern), -Infinity);
+
+    // kickoff
+    let flashes = 0;
+    let step = 0;
+    //log(`Before any steps`);
+    //table(cavernMatrix);
+
+    let flash_subtotal = 0;
+
+    // iterate
+    while (step < steps) {
+        // update all
+        cavernMatrix = increaseAll(cavernMatrix);
+
+        // find flashes
+        let flashCoordinates = findFlashes(cavernMatrix);
+        let hasFlashedMap: Map<string, boolean> = new Map();
+
+        while (flashCoordinates.length > 0) {
+            // trigger flash
+            flashCoordinates.forEach(point => {
+                hasFlashedMap.set(`${point[0]},${point[1]}`, true);
+                cavernMatrix = increaseNeighborhood({ x: point[0], y: point[1] }, cavernMatrix);
+                flash_subtotal += 1;
+            });
+
+            //look for additional flashes
+            let updatedFlashCoordinates = findFlashes(cavernMatrix);
+            flashCoordinates = [];
+            updatedFlashCoordinates.forEach(point => {
+                if (!hasFlashedMap.has(`${point[0]},${point[1]}`)) {
+                    flashCoordinates.push(point);
+                }
+            });
+        }
+
+        cavernMatrix = resetFlashed(cavernMatrix);
+        flashes += flash_subtotal;
+
+        step += 1;
+        //log(`After day ${step}`);
+        //table(cavernMatrix);
+
+        // abort! we found it
+        if (part == 2 && flash_subtotal == 100) {
+            break;
+        }
+
+        flash_subtotal = 0;
+
+    }
+
+    log(`step: ${step}`);
+    log(`flash count: ${flashes}`);
 }
 
 // main method to run the program
@@ -25,16 +121,19 @@ function main(first: boolean, second: boolean) {
         log('--------------------------------------');
         log('----------  First Challenge ----------');
         log('--------------------------------------');
-        part1();
+        //compute(1,2,'tiny.txt');
+        //compute(1,10, 'ex11.txt');
+        //compute(1,100, 'ex11.txt');
+        compute(1, 100, 'day11input.txt');
         log('\n\n');
     }
     if (second) {
         log('--------------------------------------');
         log('----------  Second Challenge ---------');
         log('--------------------------------------');
-        part2();
+        compute(2, 1000, 'day11input.txt');
         log('\n\n');
     }
 }
 
-main(true, false);
+main(true, true);
