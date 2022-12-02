baa1c25f7317c9ac6461cc09c261172cdb4a0704
eric-fleming <ericfleming8@gmail.com>
2022-12-01 23:40:41 -0600

    day2 complete

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -1,15 +1,15 @@
+"use strict";
+exports.__esModule = true;
 /*Dependent Modules*/
-import { readInput, inputToArray, inputToNumberArray, inputToStringArray } from '../shared/common';
-const log = console.log;
-const table = console.table;
-
-const rawInput: string[] = inputToStringArray('input-day2.txt', '\n');
+var common_1 = require("../shared/common");
+var log = console.log;
+var table = console.table;
+var rawInput = (0, common_1.inputToStringArray)('input-day2.txt', '\n');
 //console.table(rawInput);
-
 //  A   X   ROCK
 //  B   Y   PAPER
 //  C   Z   SISSORS
-let playMap = new Map();
+var playMap = new Map();
 playMap.set('A X', 3);
 playMap.set('A Y', 6);
 playMap.set('A Z', 0);
@@ -19,33 +19,27 @@ playMap.set('B Z', 6);
 playMap.set('C X', 6);
 playMap.set('C Y', 0);
 playMap.set('C Z', 3);
-
-
 //  A   ROCK    X   LOSE
 //  B   PAPER   Y   DRAW
 //  C   SISSORS Z   WIN
-let outcomeMap = new Map();
-outcomeMap.set('A X', 3);
-outcomeMap.set('A Y', 1);
-outcomeMap.set('A Z', 2);
-
-outcomeMap.set('B X', 1);
-outcomeMap.set('B Y', 2);
-outcomeMap.set('B Z', 3);
-
-outcomeMap.set('C X', 2);
-outcomeMap.set('C Y', 3);
-outcomeMap.set('C Z', 1);
-
+var winMap = new Map();
+winMap.set('A X', 3);
+winMap.set('A Y', 1);
+winMap.set('A Z', 2);
+winMap.set('B X', 1);
+winMap.set('B Y', 2);
+winMap.set('B Z', 3);
+winMap.set('C X', 2);
+winMap.set('C Y', 3);
+winMap.set('C Z', 1);
 //14224
-function part1(){
+function part1() {
     //log(rawInput);
-
-    let score = 0;
-    for(let k=0; k<rawInput.length-1;k++){
+    var score = 0;
+    for (var k = 0; k < rawInput.length - 1; k++) {
         //log(playMap.get(rawInput[k]))
         score += playMap.get(rawInput[k]);
-        if (rawInput[k].indexOf("X") > 0){
+        if (rawInput[k].indexOf("X") > 0) {
             score += 1;
         }
         else if (rawInput[k].indexOf("Y") > 0) {
@@ -55,18 +49,15 @@ function part1(){
             score += 3;
         }
     }
-    
-    let answer;
-    log(`total`)
-    log(`${score}`);
+    var answer;
+    log("total");
+    log("".concat(score));
 }
-
 function part2() {
-
-    let score = 0;
-    for (let k = 0; k < rawInput.length - 1; k++) {
-        //log(outcomeMap.get(rawInput[k]))
-        score += outcomeMap.get(rawInput[k]);
+    var score = 0;
+    for (var k = 0; k < rawInput.length - 1; k++) {
+        //log(winMap.get(rawInput[k]))
+        score += winMap.get(rawInput[k]);
         if (rawInput[k].indexOf("X") > 0) {
             score += 0;
         }
@@ -77,14 +68,12 @@ function part2() {
             score += 6;
         }
     }
-
-    let answer;
-    log(`total`)
-    log(`${score}`);
+    var answer;
+    log("total");
+    log("".concat(score));
 }
-
 // main method to run the program
-function main(first: boolean, second: boolean) {
+function main(first, second) {
     if (first) {
         log('--------------------------------------');
         log('----------  First Challenge ----------');
@@ -100,5 +89,4 @@ function main(first: boolean, second: boolean) {
         log('\n\n');
     }
 }
-
 main(false, true);
